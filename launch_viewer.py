19336443489374f66b561f2e48a2369734e3c246
Marco Carbone <marco@Hyena.local>
2025-07-16 17:48:11 +0200

    pausiamo BCI Competition

---
+++ launch_viewer.py
@@ -0,0 +1,15 @@
+
+import mne
+
+raw = mne.io.read_raw_gdf("A01T.gdf", preload=True)
+raw.filter(1., 40.).notch_filter(50.)
+raw.plot(block=True, n_channels=22, title="BCIC IV-2a")
+
+"""
+import mne
+raw_E = mne.io.read_raw_gdf("A01E.gdf", preload=True)
+
+# Facoltativo: filtri come per il training
+raw_E.filter(1., 40.).notch_filter(50.)
+raw_E.plot(block=True, n_channels=22, title="BCIC IV-2a")
+"""
