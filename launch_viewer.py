2dd12a345af28dc32d624865ffb81af78ffe4666
Markom.x <marcocarbonebdi@gmail.com>
2025-07-16 17:55:10 +0200

    Fixing name part 2

--- launch_viewer.py
+++ launch_viewer.py
@@ -8,7 +8,6 @@ raw.plot(block=True, n_channels=22, title="BCIC IV-2a")
 """
 import mne
 raw_E = mne.io.read_raw_gdf("A01E.gdf", preload=True)
-
 raw_E.filter(1., 40.).notch_filter(50.)
 raw_E.plot(block=True, n_channels=22, title="BCIC IV-2a")
 """
